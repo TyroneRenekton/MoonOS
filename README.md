038cd0cd183e1ae4e5faedbbd116249a190bb6a5
parallels <parallels@kali-gnu-linux-2023.2>
2025-01-04 10:52:23 +0800

    文件调整“

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ i686-linux-gnu-ld main.o -Ttext 0xc0001500 -e main -o kernel.bin
 1. 字符串搬运指令，movsb、movsw、movsd，其中movs代表move string，b代表byte、w代表word、d代表double word; 这个指令会将DS:[ESI], 搬运到ES:[EDI],
 rep 指令用于重复执行指令，使用前需要将ecx赋值
 每次执行后自动更新esi与edi，需要使用cld命令，clean direction，该指令会将eflags寄存器中的放行标志位DF置0,这样rep在movs以后，会自动加上搬运所需要的字节数; 而std表示 set direction会将df置为1, 会减掉字节
-
+2. 系统调用传参方式
+eax 存放子功能号
+ebx、ecx、edx、esi、edi 存放1-5个参数（当参数数量小于等于5时使用）
+使用ebx存放内存地址，将参数放在ebx开始的内存中（参赛数量大于5时使用）
 猜想
 1. 阅读elf文件
 根据程序头header，设置选择子以及段基址和内存区域
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-nasm -I boot.inc -o mbr.bin mbr.S
-nasm -I boot.inc -o loader.bin loader.S
-i686-linux-gnu-gcc -m32 -c -o main.o main.c
-i686-linux-gnu-ld main.o -Ttext 0xc0001500 -e main -o kernel.bin 
-dd if=loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
-dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
-dd if=kernel.bin of=hd60M.img bs=512 count=200 seek=9 conv=notrunc
+nasm -I boot/include -o boot/mbr.bin boot/mbr.S
+nasm -I boot/include -o boot/loader.bin boot/loader.S
+i686-linux-gnu-gcc -m32 -c -o kernel/main.o kernel/main.c
+i686-linux-gnu-ld kernel/main.o -Ttext 0xc0001500 -e main -o kernel/kernel.bin 
+dd if=boot/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
+dd if=boot/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+dd if=kernel/kernel.bin of=hd60M.img bs=512 count=200 seek=9 conv=notrunc
 ./bochs -f bochsrc.disk
